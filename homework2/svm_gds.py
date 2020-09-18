0884610ec47eb8165869d83aaba7e87b466650cc
Roman Alberto Velez Jimenez <rvelezji@itam.mx>
2020-09-18 14:30:13 -0500

    Added stochastic gd, included test in the .ipynb of the latter and minor aesthetic corrections to svm code

--- homework2/svm_gds.py
+++ homework2/svm_gds.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 17 19:49:27 2020
-@author: Ryo
+Created on Fri Sep 18 13:56:06 2020
 
+@author: Ryo
+Stochastic gradient descendent
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -33,12 +34,11 @@ def J_loss(theta, theta0, X, Y, lambd):
     return(J)
 
 
-def svm_gradient(X, Y, rho, lambd, tol, max_iter):
+def svm_sgd(X, Y, rho, lambd, tol, max_iter):
     # fixed values
     #number of observations and number of variables
     p = np.shape(X)[1]
-    # tuple of row vectors of the matrix
-    X_tuple = tuple(X)
+    n = np.shape(X)[0]
         
     # initialize
     theta0 = np.array(0).reshape(1)
@@ -47,13 +47,23 @@ def svm_gradient(X, Y, rho, lambd, tol, max_iter):
     
     # save data
     J_vec = np.array(0)
-    J_vec = np.append(J_vec, J_loss(theta[k], theta0[k], X, Y, lambd) )
+    J_vec = np.append(J_vec, J_loss(theta[k], theta0[k], X, Y, lambd))
     
     while abs(J_vec[k+1] - J_vec[k]) > tol and k < max_iter:
+        # stochastic index 
+        index_batch = range(1, n)
+        size_stochastic = np.random.choice(index_batch)
+        j = list(np.random.choice(index_batch, size=size_stochastic))
+        
+        #subset stochastic subset
+        X_stochastic = X[j]
+        Y_stochastic = Y[j]
+        Xs_tuple = tuple(X_stochastic)
         
         # inner operations
-        loss01_y = np.array([yi * loss_01(theta[k], theta0[k], xi, yi) for xi, yi in zip(X_tuple,Y)])
-        vec_LyX = (X.T * loss01_y).T
+        loss01_y = np.array([yi * loss_01(theta[k], theta0[k], xi, yi) 
+                             for xi, yi in zip(Xs_tuple,Y_stochastic)])
+        vec_LyX = (X_stochastic.T * loss01_y).T
         
         # update
         thet = theta[k] - rho * (np.mean(vec_LyX, axis=0) + lambd * theta[k])
@@ -75,51 +85,4 @@ def svm_gradient(X, Y, rho, lambd, tol, max_iter):
     
     return(theta, theta0, k)
         
-
-# =============================================================================
-#   TESTS
-# =============================================================================
-# np.random.seed(1)
-# x_test = np.random.randn(6,2) - 2
-# x_test = np.vstack((x_test, np.random.randn(4,2) + 2))
-
-# y_test = np.array([-1 if x < 6 else 1 for x in range(10)])
-
-# theta0_test = np.random.random()
-# theta_test = np.random.randn(2)
-# lambd_test = 1
-# rho_test = 0.1
-# tol_test = 1e-3
-# max_iter = 100
-
-# # test SVM Gradient (is ok)
-# th_ans, th0_ans, k_ans = svm_gradient(x_test, y_test, rho_test, lambd_test, tol_test, max_iter)
-
-# # # look data and SVM
-# x = x_test[:,0]
-# y = x_test[:,1]
-# t = np.linspace(x.min(), x.max())
-
-# fig = plt.figure()
-# ax = fig.add_subplot()
-# ax.scatter(x, y, c=y_test)
-# ax.plot(t, -(th_ans[k_ans][0] * t + th0_ans[k_ans]) / th_ans[k_ans][1], color='lightblue')
-# plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
